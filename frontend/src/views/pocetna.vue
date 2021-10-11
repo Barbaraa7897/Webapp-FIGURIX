e14e1dd316164a8ed3de568e786717b71a472b24
Barbara Zeljeznik <barbara.zeljeznik@gmail.com>
2021-10-11 16:29:28 +0200

    gotov projekt

---
+++ frontend/src/views/pocetna.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+  <div class="pocetna">
+    <h1>Dobro došli!</h1>
+    <img src="@/assets/boardgames.jpg" class="slika" />
+  </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+ 
+}
+</script>
+<style scoped>
+.slika{
+  width:100%;
+  height:500px;
+}
+</style>
